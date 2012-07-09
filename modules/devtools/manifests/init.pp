29bb091b5a416e19ce1e846255c873d1c0afa7bf
Eric di Domenico <eric@didomenico.com>
2012-07-08 18:56:25 -0700

    Add Ruby versions and vpn

--- modules/devtools/manifests/init.pp
+++ modules/devtools/manifests/init.pp
@@ -4,9 +4,54 @@ class devtools {
       ensure => installed;
     "build-essential":
       ensure => installed;
-    "firefox":
+    "python-all-dev":
       ensure => installed;
-    "openjdk-7-jdk":
+    "gcc-4.4-multilib":
       ensure => installed;
+    "tmux":
+      ensure => installed;
+    "ubuntu-desktop":
+      ensure => installed;
+    "virtualbox-guest-x11":
+      ensure => installed;
+    "icedtea6-plugin":
+      ensure => installed;
+    "ia32-libs":
+      ensure => installed;
+  }
+
+  rvm_system_ruby {
+    "ruby-1.9.2-p290":
+      ensure => "present",
+      default_use => true;
+    "ruby-1.8.7-p357":
+      ensure => "present",
+      default_use => true;
+  }
+
+  file {
+    "/home/vagrant/vpn.tgz":
+      source => "puppet:///modules/devtools/vpn.tgz";
+  }
+
+  exec {
+    "untar vpn":
+      alias => "untar vpn",
+      command => "/bin/tar xzf vpn.tgz",
+      require => [Package["ia32-libs"], File["/home/vagrant/vpn.tgz"]],
+      cwd => "/home/vagrant",
+      user => root,
+      creates => "/home/vagrant/.juniper_networks";
+    "clone juniperprompt":
+      alias => "clone juniperprompt",
+      command => "/usr/bin/git clone https://github.com/crimsonknave/juniperncprompt.git",
+      require => [Package["git"]],
+      cwd => "/home/vagrant",
+      creates => "/home/vagrant/juniperncprompt";
+    "install juniperprompt dependencies":
+      alias => "install juniperprompt dependencies",
+      command => "/usr/bin/python setup.py install",
+      require => [File["/home/vagrant/juniperncprompt"]]
+      cwd => "/home/vagrant/juniperncprompt/elementtidy-1.0-20050212";
   }
 }
